a5af106b6ea16d0bf915197f930b8849e1022c2f
Victor Gorgal <victorgorgal@gmail.com>
2024-05-21 00:33:57 -0300

    feat: Add input shift register

--- main.c
+++ main.c
@@ -3,20 +3,32 @@
 #include "hardware/i2c.h"
 #include "hardware/pio.h"
 #include "hardware/watchdog.h"
-#include "shift_register_in.pio.h"
-#include "shift_register_out.pio.h"
+#include "shift_register/shift_register.c"
 
 ShiftRegister shiftRegister;
 ShiftRegister shiftRegister2;
 
-void test_shift_register() {
-    uint8_t data_in[shiftRegister.registerCount];
-    for (int i = 0; i < shiftRegister.registerCount; i++) {
-        data_in[i] = 0;
+void print_bits(uint32_t data, uint8_t dataSize) {
+    for (uint8_t i = 0; i < dataSize; i++) {
+        printf("%d", (data >> (dataSize-1-i)) & 1);
     }
+}
+
+void test_shift_register() {
+    uint32_t data_in[1];
 
     read_from_shift_register(&shiftRegister2, data_in);
-    write_to_shift_register(&shiftRegister, data_in);
+
+    printf("------------\n");
+    // for (int i = 0; i < 1; i++) {
+    print_bits(data_in[0], 32);
+    // }
+    printf("\n");
+
+    // data_in[0] = 0b11110111;
+    // data_in[1] = 0b00011000;
+    // data_in[2] = 0b10101011;
+    // write_to_shift_register(&shiftRegister, data_in);
 }
 
 void init_shift_registers() {
@@ -24,32 +36,37 @@ void init_shift_registers() {
     shiftRegister.sm = pio_claim_unused_sm(shiftRegister.pio, true);
     shiftRegister.dataPin = 12;
     shiftRegister.clockPin = 13;
-    shiftRegister.registerCount = 2;
+    shiftRegister.registerCount = 1;
 
     uint offset = pio_add_program(shiftRegister.pio, &shift_register_out_program);
-    init_out_shift_register(&shiftRegister, offset, 1 * 1000);
+    init_out_shift_register(&shiftRegister, offset, 1 * 1 * 1000);
 
     shiftRegister2.pio = pio0;
     shiftRegister2.sm = pio_claim_unused_sm(shiftRegister2.pio, true);
-    shiftRegister2.dataPin = 14;
-    shiftRegister2.clockPin = 15;
-    shiftRegister2.registerCount = 2;
+    shiftRegister2.dataPin = 10;
+    shiftRegister2.clockPin = 13;
+    shiftRegister2.registerCount = 1;
 
     uint offset2 = pio_add_program(shiftRegister2.pio, &shift_register_in_program);
-    init_in_shift_register(&shiftRegister2, offset2, 1 * 1000);
+    init_in_shift_register(&shiftRegister2, offset2, 1 * 1 * 1000);
 }
 
 int main() {
     stdio_init_all();
 
     // Watchdog
-    watchdog_enable(2000, true);
+    watchdog_enable(5000, true);
+
+    gpio_init(24);
+    gpio_set_dir(24, false);
+    gpio_set_pulls(24, true, false);
 
     init_shift_registers();
-    
     while (true) {
         sleep_ms(500);
-        test_shift_register();
+        if (!gpio_get(24)) {
+            test_shift_register();
+        }
         watchdog_update();
     }
 }
---
+++ shift_register/shift_register.c
@@ -0,0 +1,93 @@
+#include "hardware/clocks.h"
+#include "hardware/dma.h"
+#include "shift_register_in.pio.h"
+#include "shift_register_out.pio.h"
+
+volatile uint32_t data = 0;
+int dma_chan = 0;
+
+typedef struct ShiftRegister {
+    PIO pio;
+    uint sm;
+    uint8_t registerCount;
+    uint8_t dataPin;
+    uint8_t clockPin;
+} ShiftRegister;
+
+
+// SIPO
+// Max freq. 30MHz
+void init_out_shift_register(ShiftRegister *shiftRegister, uint offset, float clock) {
+    clock *= shift_register_out_program.length;
+    float clockDiv = (float) clock_get_hz(clk_sys) / clock;
+    pio_sm_config c = shift_register_out_program_get_default_config(offset);
+
+    pio_gpio_init(shiftRegister->pio, shiftRegister->dataPin);
+    pio_gpio_init(shiftRegister->pio, shiftRegister->clockPin);
+
+    sm_config_set_out_pins(&c, shiftRegister->dataPin, 1);
+    pio_sm_set_consecutive_pindirs(shiftRegister->pio, shiftRegister->sm, shiftRegister->dataPin, 1, true);
+    sm_config_set_sideset_pins(&c, shiftRegister->clockPin);
+    pio_sm_set_consecutive_pindirs(shiftRegister->pio, shiftRegister->sm, shiftRegister->clockPin, 1, true);
+
+    sm_config_set_out_shift(&c, true, true, 8);
+
+    sm_config_set_clkdiv(&c, clockDiv);
+    pio_sm_init(shiftRegister->pio, shiftRegister->sm, offset, &c);
+    pio_sm_set_enabled(shiftRegister->pio, shiftRegister->sm, true);
+}
+
+// PISO
+void init_in_shift_register(ShiftRegister *shiftRegister, uint offset, float clock) {
+    PIO pio = shiftRegister->pio;
+    uint sm = shiftRegister->sm;
+
+    clock *= shift_register_in_program.length;
+    float clockDiv = (float) clock_get_hz(clk_sys) / clock;
+    pio_sm_config c = shift_register_in_program_get_default_config(offset);
+
+    pio_gpio_init(pio, shiftRegister->dataPin);
+    pio_gpio_init(pio, shiftRegister->clockPin);
+
+    sm_config_set_in_pins(&c, shiftRegister->dataPin);
+    pio_sm_set_consecutive_pindirs(pio, sm, shiftRegister->dataPin, 1, false);
+    sm_config_set_sideset_pins(&c, shiftRegister->clockPin);
+    pio_sm_set_consecutive_pindirs(pio, sm, shiftRegister->clockPin, 1, true);
+
+    sm_config_set_in_shift(&c, false, false, 8);
+
+    sm_config_set_clkdiv(&c, clockDiv);
+    // pio_sm_clear_fifos(pio, sm);
+    // pio_sm_restart(pio, sm);
+    // hw_set_bits(&pio->input_sync_bypass, 1u << shiftRegister->dataPin);
+
+    pio_sm_init(pio, sm, offset, &c);
+
+    dma_channel_config dc = dma_channel_get_default_config(dma_chan);
+    channel_config_set_read_increment(&dc, false);
+    channel_config_set_write_increment(&dc, false);
+    channel_config_set_dreq(&dc, pio_get_dreq(shiftRegister->pio, shiftRegister->sm, false));
+    channel_config_set_transfer_data_size( &dc, DMA_SIZE_8 );
+
+    dma_channel_configure(dma_chan, &dc,
+            &data,                    // Destination pointer (local var)
+            &shiftRegister->pio->rxf[shiftRegister->sm],           // Source pointer (receive from PIO FIFO)
+            1,                                  // Number of transfers
+            true);
+            
+    pio_sm_set_enabled(pio, sm, true);
+}
+
+void write_to_shift_register(ShiftRegister *shiftRegister, uint32_t *dataArray) {
+    for (uint8_t i = 0; i < shiftRegister->registerCount; i++) {
+        pio_sm_put_blocking(shiftRegister->pio, shiftRegister->sm, dataArray[i]);
+    }
+}
+
+void read_from_shift_register(ShiftRegister *shiftRegister, uint32_t dataArray[]) {
+    // dataArray[0] = pio_sm_get(shiftRegister->pio, shiftRegister->sm);
+    pio_sm_put_blocking(shiftRegister->pio, shiftRegister->sm, 1);
+    dma_channel_wait_for_finish_blocking(dma_chan);
+    dataArray[0] = data;
+    dma_channel_start(dma_chan);
+}
