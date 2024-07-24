4b47b417f0216c632b626be6a97619a76010240b
Nathália Aparecida <nathaliaaparecida1804@gmail.com>
2024-07-24 15:36:26 -0300

    feat: Add stuff

--- flash_lib/src/flash_lib.c
+++ flash_lib/src/flash_lib.c
@@ -1,17 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 #include "hardware/flash.h"
 #include "hardware/sync.h"
 #include "flash_lib.h"
 
 #define MEMORY_SIGNATURE 0x27062021
 #define SIGNATURE_SIZE_BYTES 4
+#define SIGNATURE_POSITION 0
+#define LOGICAL_ID_POSITION 1
+#define WRITE_COUNT_POSITION 2
+#define PHYSICAL_ID_POSITION 3
 
 typedef struct SectorHeader {
     uint32_t signature;
     uint16_t logicalID;
     uint16_t writeCount;
+    uint8_t id;
 } SectorHeader;
 
 uint32_t _lower_bound;
@@ -25,6 +31,12 @@ void init_sectors();
 void _write_sector_by_physical_addr(uint32_t physical_sector_address, const uint8_t *data);
 void delete_sectors(uint32_t begin, uint32_t end);
 void delete_sector(uint32_t physical_sector);
+uint32_t get_header_attribute_from_sector(uint32_t physical_sector, uint8_t attribute_id);
+bool get_first_sector_from_logical_id(uint16_t logical_id, uint32_t *physical_addr);
+bool get_physical_sector_from_logical_id(uint16_t logical_id, uint8_t physical_sector_id, uint32_t *physical_addr);
+uint32_t get_memory_addr_from_physical_sector(uint32_t physical_sector);
+void prepare_buffer_to_write(uint8_t *buffer, const void *data, uint8_t data_size);
+void read_and_update_header(uint32_t physical_sector_id, SectorHeader *sectorHeader);
 
 /**
  * @brief Initializes the flash memory library.
@@ -81,7 +93,7 @@ void init_sectors() {
 
     // Checks every logical ID to know which ones needs initialization
     for (uint32_t logical_id = 0; logical_id < _logical_sectors_count; logical_id++) {
-        bool found_sector = get_physical_sector_from_logical_id(logical_id, NULL);
+        bool found_sector = get_first_sector_from_logical_id(logical_id, NULL);
 
         if (found_sector) {
             continue;
@@ -105,38 +117,84 @@ void init_sectors() {
     }
 }
 
-/**
- * @brief Retrieves the pointer to the sector
- * 
- * @param sector logical sector ID to read
- * 
- * @return pointer to the memory address
-*/
-uint8_t * read_sector(uint16_t sector) {
+uint8_t * read_sector(uint16_t logical_sector, uint32_t offset_bytes) {
     uint32_t physical_sector_address;
-    get_physical_sector_from_logical_id(sector, &physical_sector_address);
-    return get_sector_read_pointer(physical_sector_address);
+    uint32_t physical_sector_id = offset_bytes / FLASH_SECTOR_SIZE;
+    uint32_t physical_sector_offset = offset_bytes % FLASH_SECTOR_SIZE;
+    get_physical_sector_from_logical_id(logical_sector, physical_sector_id, &physical_sector_address);
+    return (uint8_t *) (get_memory_addr_from_physical_sector(physical_sector_address + physical_sector_offset) + XIP_BASE);
 }
 
-void write_sector(uint16_t sector, uint32_t logical_sector_offset, const uint8_t *data, uint32_t count) {
+void erase_logical_sector(uint16_t logical_sector) {
+    assert(logical_sector < _logical_sectors_count);
+
+    uint32_t irq_status = save_and_disable_interrupts();
+
+    SectorHeader *sectorHeaders = (SectorHeader *) malloc(_group_by * sizeof(SectorHeader));
+
+    for (uint8_t i = 0; i < _group_by; ++i) {
+        uint32_t physical_sector_address;
+        get_physical_sector_from_logical_id(logical_sector, i, &physical_sector_address);
+        read_and_update_header(physical_sector_address, &sectorHeaders[i]);
+    }
+
     uint32_t physical_sector_address;
-    get_physical_sector_from_logical_id(sector, &physical_sector_address);
+    get_first_sector_from_logical_id(logical_sector, &physical_sector_address);
+    uint32_t memory_addr = get_memory_addr_from_physical_sector(physical_sector_address);
+    flash_range_erase(memory_addr, FLASH_SECTOR_SIZE * _group_by);
+
+    for (uint8_t i = 0; i < _group_by; ++i) {
+        uint8_t headerBuffer[FLASH_PAGE_SIZE];
+        prepare_buffer_to_write(headerBuffer, &sectorHeaders[i], sizeof(SectorHeader));
+
+        get_physical_sector_from_logical_id(logical_sector, i, &physical_sector_address);
+        memory_addr = get_memory_addr_from_physical_sector(physical_sector_address);
+        flash_range_program(memory_addr, headerBuffer, FLASH_PAGE_SIZE);
+    }
+
+    free(sectorHeaders);
+    restore_interrupts(irq_status);
+}
+
+void erase_physical_sector(uint16_t logical_sector, uint8_t physical_sector_id) {
+    assert(logical_sector < _logical_sectors_count);
+    assert(physical_sector_id < _group_by);
+
+    uint32_t irq_status = save_and_disable_interrupts();
+
+    uint32_t physical_sector_address;
+    get_physical_sector_from_logical_id(logical_sector, physical_sector_id, &physical_sector_address);
+    uint32_t memory_addr = get_memory_addr_from_physical_sector(physical_sector_address);
+    flash_range_erase(memory_addr, FLASH_SECTOR_SIZE);
 
     SectorHeader sectorHeader;
     uint8_t headerBuffer[FLASH_PAGE_SIZE];
     read_and_update_header(physical_sector_address, &sectorHeader);
     prepare_buffer_to_write(headerBuffer, &sectorHeader, sizeof(SectorHeader));
-    memcpy(headerBuffer + sizeof(SectorHeader), data, count);
-
-    uint32_t memory_addr = get_memory_addr_from_physical_sector(physical_sector_address);
-
-    uint32_t irq_status = save_and_disable_interrupts();
-
-    flash_range_erase(memory_addr, FLASH_SECTOR_SIZE);
     flash_range_program(memory_addr, headerBuffer, FLASH_PAGE_SIZE);
 
     restore_interrupts(irq_status);
 }
+
+// void write_sector(uint16_t sector, uint32_t logical_sector_offset, const uint8_t *data, uint32_t count) {
+//     uint32_t physical_sector_address;
+//     get_physical_sector_from_logical_id(sector, &physical_sector_address);
+
+//     SectorHeader sectorHeader;
+//     uint8_t headerBuffer[FLASH_PAGE_SIZE];
+//     read_and_update_header(physical_sector_address, &sectorHeader);
+//     prepare_buffer_to_write(headerBuffer, &sectorHeader, sizeof(SectorHeader));
+//     memcpy(headerBuffer + sizeof(SectorHeader), data, count);
+
+//     uint32_t memory_addr = get_memory_addr_from_physical_sector(physical_sector_address);
+
+//     uint32_t irq_status = save_and_disable_interrupts();
+
+//     flash_range_erase(memory_addr, FLASH_SECTOR_SIZE);
+//     flash_range_program(memory_addr, headerBuffer, FLASH_PAGE_SIZE);
+
+//     restore_interrupts(irq_status);
+// }
 
 uint32_t get_header_attribute_from_sector(uint32_t physical_sector, uint8_t attribute_id) {
     uint8_t *read_pointer = get_sector_read_pointer(physical_sector);
@@ -145,30 +203,51 @@ uint32_t get_header_attribute_from_sector(uint32_t physical_sector, uint8_t attr
         memcpy(&attribute, read_pointer, SIGNATURE_SIZE_BYTES);
     } else if (attribute_id == 1) {
         memcpy(&attribute, read_pointer + SIGNATURE_SIZE_BYTES, sizeof(uint16_t));
-    } else {
+    } else if (attribute_id == 2) {
         memcpy(&attribute, read_pointer + SIGNATURE_SIZE_BYTES + sizeof(uint16_t), sizeof(uint16_t));
+    } else {
+        memcpy(&attribute, read_pointer + SIGNATURE_SIZE_BYTES + 2 * sizeof(uint16_t), sizeof(uint8_t));
     }
     return attribute;
 }
 
 bool check_sector_signature(uint32_t physical_sector) {
-    return get_header_attribute_from_sector(physical_sector, 0) == MEMORY_SIGNATURE;
+    return get_header_attribute_from_sector(physical_sector, SIGNATURE_POSITION) == MEMORY_SIGNATURE;
 }
 
-bool get_physical_sector_from_logical_id(uint16_t logical_id, uint32_t *physical_addr) {
+bool get_first_sector_from_logical_id(uint16_t logical_id, uint32_t *physical_addr) {
     for (uint32_t physical_sector = _lower_bound; physical_sector < _upper_bound; physical_sector += _group_by) {
         if (!check_sector_signature(physical_sector)) {
             continue;
         }
 
-        if (get_header_attribute_from_sector(physical_sector, 1) == logical_id) {
+        if (get_header_attribute_from_sector(physical_sector, LOGICAL_ID_POSITION) == logical_id) {
             if (physical_addr != NULL) {
                 *physical_addr = physical_sector;
             }
             return true;
         }
     }
+
     return false;
+}
+
+bool get_physical_sector_from_logical_id(uint16_t logical_id, uint8_t physical_sector_id, uint32_t *physical_addr) {
+    uint32_t physical_sector;
+    bool found = get_first_sector_from_logical_id(logical_id, &physical_sector);
+
+    for (uint8_t i = 0; i < _group_by; ++i) {
+        if (get_header_attribute_from_sector(physical_sector, PHYSICAL_ID_POSITION) == physical_sector_id) {
+            break;
+        }
+
+        physical_sector += FLASH_SECTOR_SIZE;
+    }
+
+    if (physical_addr != NULL) {
+        *physical_addr = physical_sector;
+    }
+    return found;
 }
 
 void prepare_buffer_to_write(uint8_t *buffer, const void *data, uint8_t data_size) {
@@ -284,7 +363,9 @@ void flash_lib_example() {
 
     uint16_t sector_to_write = 0;
     uint32_t my_physical_sector;
+    uint8_t writeBuffer[FLASH_PAGE_SIZE];
     uint8_t data_to_write[4] = {0x0A, 0xFA, 0xCA, 0xDA};
+    prepare_buffer_to_write(writeBuffer, data_to_write, 4);
 
     uint16_t lower_bound = 100;
     uint16_t sectors_count = 10;
@@ -297,32 +378,10 @@ void flash_lib_example() {
     elapsed_time = 1.0*absolute_time_diff_us(start_time, end_time)/1000;
     printf("Time to init library: %.3fms\n", elapsed_time);
 
-    print_sector_header();
 
     start_time = get_absolute_time();
     // *** Code ***
-    delete_all_sectors();
-    // *** Code ***
-    end_time = get_absolute_time();
-    elapsed_time = 1.0*absolute_time_diff_us(start_time, end_time)/1000;
-    printf("Time to init library: %.3fms\n", elapsed_time);
-
-    print_sector_header();
-
-    start_time = get_absolute_time();
-    // *** Code ***
-    init_flash_lib(lower_bound, sectors_count, GROUP_BY_1);
-    // *** Code ***
-    end_time = get_absolute_time();
-    elapsed_time = 1.0*absolute_time_diff_us(start_time, end_time)/1000;
-    printf("Time to init library: %.3fms\n", elapsed_time);
-    
-    print_sector_header();
-
-
-    start_time = get_absolute_time();
-    // *** Code ***
-    get_physical_sector_from_logical_id(sector_to_write, &my_physical_sector);
+    get_first_sector_from_logical_id(sector_to_write, &my_physical_sector);
     // *** Code ***
     end_time = get_absolute_time();
     elapsed_time = 1.0*absolute_time_diff_us(start_time, end_time);
@@ -330,18 +389,41 @@ void flash_lib_example() {
     printf("Logical id: %d At physical addr at: %d\n", sector_to_write, my_physical_sector);
 
 
+    // start_time = get_absolute_time();
+    // // *** Code ***
+    // // write_sector(sector_to_write, 0, data_to_write, sizeof(data_to_write));
+    // uint32_t memory_addr = get_memory_addr_from_physical_sector(109);
+
+    // uint32_t irq_status = save_and_disable_interrupts();
+
+    // flash_range_erase(memory_addr, FLASH_SECTOR_SIZE);
+
+    // restore_interrupts(irq_status);
+    // // *** Code ***
+    // end_time = get_absolute_time();
+    // elapsed_time = 1.0*absolute_time_diff_us(start_time, end_time)/1000;
+    // printf("Time to erase to sector: %.2fms\n", elapsed_time);
+
+
     start_time = get_absolute_time();
     // *** Code ***
-    write_sector(sector_to_write, 0, data_to_write, sizeof(data_to_write));
+    // write_sector(sector_to_write, 0, data_to_write, sizeof(data_to_write));
+    uint32_t memory_addr = get_memory_addr_from_physical_sector(109);
+
+    uint32_t irq_status = save_and_disable_interrupts();
+
+    // flash_range_program(memory_addr, writeBuffer, FLASH_PAGE_SIZE);
+
+    restore_interrupts(irq_status);
     // *** Code ***
     end_time = get_absolute_time();
-    elapsed_time = 1.0*absolute_time_diff_us(start_time, end_time)/1000;
-    printf("Time to write to sector: %.2fms\n", elapsed_time);
+    elapsed_time = 1.0*absolute_time_diff_us(start_time, end_time);
+    printf("Time to write to sector: %.2fus\n", elapsed_time);
 
 
     start_time = get_absolute_time();
     // *** Code ***
-    get_physical_sector_from_logical_id(1 << 15, &my_physical_sector);
+    get_first_sector_from_logical_id(1 << 15, &my_physical_sector);
     // *** Code ***
     end_time = get_absolute_time();
     elapsed_time = 1.0*absolute_time_diff_us(start_time, end_time);
